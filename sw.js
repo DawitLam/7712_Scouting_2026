885f818acb6448272b4f24738312a0254728e520
DawitLam <DawitLam@users.noreply.github.com>
2026-03-16 20:40:31 -0400

    Bump cache version to v3.1.0 to fix counter buttons (adjustCounter cache miss)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Team 7712 Scouting App
-const CACHE_NAME = 'team-7712-scouting-v2.9.3';
+const CACHE_NAME = 'team-7712-scouting-v3.1.0';
 const urlsToCache = [
     './',
     '/index.html',
